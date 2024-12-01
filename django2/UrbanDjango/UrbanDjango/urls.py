5f50d4c8a3068dd99635901b973a9236d087a0ca
lonelorth <109285932+lonelorth@users.noreply.github.com>
2024-12-01 15:34:39 +0300

    Add files via upload

--- django2/UrbanDjango/UrbanDjango/urls.py
+++ django2/UrbanDjango/UrbanDjango/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from task2.views import func_templates
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', func_templates),
+    path('index/', TemplateView.as_view(template_name='second_task/class_template.html'))
 ]
